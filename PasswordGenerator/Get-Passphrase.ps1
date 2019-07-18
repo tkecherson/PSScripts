5e7683074f62a5512e062b132ae857c8191b5b75
tkecherson <50464958+tkecherson@users.noreply.github.com>
2019-07-18 15:58:27 -0400

    Updated and added -Count feature.

--- PasswordGenerator/Get-Passphrase.ps1
+++ PasswordGenerator/Get-Passphrase.ps1
@@ -1,10 +1,19 @@
-# Create Random Password
+Param (
+    [Parameter(Position=0,Mandatory=$False)]
+    [Int]$Count=1    
+)
+# Create Random Passphrase
 #Word List
 $WordList = Get-Content .\enable1.txt
- 
+
+$i = 1
+While ($i -le $count) {
 # Create Passphrase
 $W1lower = Get-Random -InputObject $WordList -Count 4
 $W1 = (Get-Culture).TextInfo.ToTitleCase($W1lower)
-$date = Get-Date
 
-Write-Host "Password: $($W1). Selected on $date."
+Write-Host "$($W1)"
+$i+=1
+}
+
+Break
--- PasswordGenerator/Get-Password.ps1
+++ PasswordGenerator/Get-Password.ps1
@@ -1,16 +1,23 @@
+Param (
+    [Parameter(Position=0,Mandatory=$False)]
+    [Int]$Count=1    
+)
 # Create Random Password
 # Adjective List
 $adjectivelist = (Get-Content ".\english-adjectives.txt")
 
 # Noun List
 $nounlist = (Get-Content ".\english-nouns.txt")
- 
+
+$i = 1
+While ($i -le $count) {
 # Select random object
 $adjective = Get-Random -InputObject $adjectivelist -Count 1
 $noun = Get-Random -InputObject $nounlist -Count 1
 $symbol = -join ((33..47) + (60..64) | Get-Random -Count 1 | ForEach-Object {[char]$_})
 $symbolnumber = -join ((33..57) + (60..64) | Get-Random -Count 1 | ForEach-Object {[char]$_})
 $number = -join ((48..57) | Get-Random -Count 1 | ForEach-Object {[char]$_})
-$date = Get-Date
 
-Write-Host "Password: $($adjective)$($noun)$($symbol)$($symbolnumber)$($number). Selected on $date."
+Write-Host "$($adjective)$($noun)$($symbol)$($symbolnumber)$($number)"
+$i+=1
+}
